d154f158d2778caa86cc2ed8c46b86e93ab6c792
Nasif-21 <nasifshitab01@gmail.com>
2023-12-28 12:44:20 +0600

    Footer done, Image work fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,19 +1,29 @@
+//import { Link } from "react-scroll";
 import "./Footer.css";
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div className="Footer">
-        <div className="Footer-container">
+    <div className="footer">
+        <div className="footer-container">
             <div className="left-part">
-                <div className="Location">
+                <div className="location">
                     <p><i className="fa-solid fa-house"></i></p>
+                    <div>
+                        <p>531/B/1, West Shewrapara, Mirpur, Dhaka, Bangladesh</p>
+                    </div>
                 </div>
                 <div className="Phone">
                     <p><i className="fa-solid fa-phone"></i></p>
+                    <div>
+                        <p>+8801799214049</p>
+                    </div>
                 </div>
                 <div className="Mail">
                     <p><i className="fa-solid fa-envelope"></i></p>
+                    <div>
+                        <p>nasifshitab01@gmail.com</p>
+                    </div>
                 </div>
             </div>
 
@@ -21,7 +31,16 @@ const Footer = () => {
 
 
 
-            <div className="right-part"></div>
+            <div className="right-part">
+                <p>Want to see my works and my social links</p>
+                       
+                <div className="social">
+                <i class="fa-brands fa-github fa-xl"  style={{color: "#ffffff", marginRight: "0.8rem" }}></i>
+                <i class="fa-brands fa-linkedin fa-xl" style={{color: "#ffffff", marginRight: "0.8rem"}}></i>
+                <i class="fa-brands fa-figma fa-xl" style={{color: "#ffffff", marginRight: "0.8rem"}}></i>
+
+                </div>
+            </div>
         </div>
 
     </div>
